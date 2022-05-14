c34a51d8f8b04648605fa773c645d135c5045b6b
Tawa <tawanicolas@gmail.com>
2022-05-14 17:13:19 +0200

    Update Filtering

--- Phoenix/Views/ComponentDependenciesPopover.swift
+++ Phoenix/Views/ComponentDependenciesPopover.swift
@@ -31,126 +31,131 @@ struct ComponentDependenciesPopover: View {
     }
     
     var body: some View {
-        ZStack {
-            VStack {
-                HStack {
-                    VStack {
-                        HStack {
-                            TextField("Filter", text: $filter)
-                                .focused($textFieldFocus)
-                                .onAppear(perform: {
-                                    textFieldFocus = true
-                                })
-                                .onExitCommand(perform: {
-                                    if filter.isEmpty {
-                                        showingPopup = false
-                                    } else {
-                                        filter = ""
-                                    }
-                                })
-                            if !filter.isEmpty {
-                                Button(action: { filter = "" }, label: {
-                                    Image(systemName: "clear.fill")
-                                })
-                                .aspectRatio(1, contentMode: .fit)
+        VStack {
+            HSplitView {
+                VStack(alignment: .leading) {
+                    HStack {
+                        TextField("Filter", text: $filter)
+                            .focused($textFieldFocus)
+                            .onAppear(perform: {
+                                textFieldFocus = true
+                            })
+                            .font(.title)
+                            .onExitCommand(perform: {
+                                if filter.isEmpty {
+                                    showingPopup = false
+                                } else {
+                                    filter = ""
+                                }
+                            })
+                        if !filter.isEmpty {
+                            Button(action: { filter = "" }, label: {
+                                Image(systemName: "clear.fill")
+                            })
+                            .aspectRatio(1, contentMode: .fit)
+                        }
+                    }.padding(16)
+                    List {
+                        Text("Components:")
+                            .font(.largeTitle)
+                        let filteredNames = Dictionary(grouping: store.allNames.filter { name in
+                            if !filter.isEmpty && !name.full.lowercased().contains(filter.lowercased()) {
+                                return false
                             }
-                        }.padding()
-                        ScrollView {
-                            HStack {
-                                VStack(alignment: .leading) {
-                                    Text("Components:")
-                                        .font(.largeTitle)
-                                    ForEach(store.allNames.filter { name in
-                                        if !filter.isEmpty && !name.full.lowercased().contains(filter.lowercased()) {
-                                            return false
-                                        }
-                                        return store.selectedName != name && !store.selectedComponentDependenciesContains(dependencyName: name)
-                                    }) { name in
-                                        Button {
-                                            store.addDependencyToSelectedComponent(dependencyName: name)
-                                            showingPopup = false
-                                        } label: {
-                                            Text("\(name.family): \(store.title(for: name))")
-                                        }
+                            return store.selectedName != name && !store.selectedComponentDependenciesContains(dependencyName: name)
+                        }, by: { $0.family })
+                        ForEach(filteredNames.keys.sorted(), id: \.self) { familyName in
+                            Section {
+                                ForEach(filteredNames[familyName]!) { name in
+                                    Button {
+                                        store.addDependencyToSelectedComponent(dependencyName: name)
+                                        showingPopup = false
+                                    } label: {
+                                        Text((store.title(for: name)))
                                     }
                                 }
-                                Spacer()
+                            } header: {
+                                Text(familyName)
+                                    .font(.title)
                             }
                         }
-                    }.frame(width: 400)
-                    Divider()
-                    ScrollView {
-                        VStack(alignment: .leading, spacing: 0) {
-                            Text("External Dependency:")
-                                .font(.largeTitle)
+                        Spacer()
+                    }
+                    .listStyle(SidebarListStyle())
+                    .padding(.horizontal)
+                }.frame(width: 400)
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text("External Dependency:")
+                            .font(.largeTitle)
+                        HStack {
                             HStack {
-                                HStack {
-                                    Text("URL:")
-                                        .font(.title)
-                                    Spacer()
-                                }
-                                .frame(width: 100)
-                                TextField("ex: git@github.com:team/repo.git", text: $externalURL)
+                                Text("URL:")
                                     .font(.title)
-                                    .padding()
+                                Spacer()
                             }
-                            
-                            HStack {
-                                HStack {
-                                    Menu {
-                                        Button(action: {
-                                            externalDescription = .from(version: "")
-                                        }, label: { Text("from") })
-                                        Button(action: {
-                                            externalDescription = .branch(name: "")
-                                        }, label: { Text("branch") })
-                                    } label: {
-                                        Text(externalDescriptionMenuTitle)
-                                    }
-                                }.frame(width: 100)
-                                TextField(externalDescriptionTextPlaceholder, text: Binding(get: { externalDescriptionTextValue }, set: { value in
-                                    switch externalDescription {
-                                    case .from:
-                                        externalDescription = .from(version: value)
-                                    case .branch:
-                                        externalDescription = .branch(name: value)
-                                    }
-                                }))
+                            .frame(width: 100)
+                            TextField("ex: git@github.com:team/repo.git", text: $externalURL)
                                 .font(.title)
                                 .padding()
-                                Spacer()
-                            }
-                            
+                        }
+
+                        HStack {
                             HStack {
                                 Menu {
-                                    Button(action: setExternalNameAsName, label: { Text("Name") })
-                                    Button(action: setExternalNameAsProduct, label: { Text("product") })
+                                    Button(action: {
+                                        externalDescription = .from(version: "")
+                                    }, label: { Text("from") })
+                                    Button(action: {
+                                        externalDescription = .branch(name: "")
+                                    }, label: { Text("branch") })
                                 } label: {
-                                    Text(externalNameMenuTitle)
+                                    Text(externalDescriptionMenuTitle)
                                 }
-                                .frame(width: 100)
+                            }.frame(width: 100)
+                            TextField(externalDescriptionTextPlaceholder, text: Binding(get: { externalDescriptionTextValue }, set: { value in
+                                switch externalDescription {
+                                case .from:
+                                    externalDescription = .from(version: value)
+                                case .branch:
+                                    externalDescription = .branch(name: value)
+                                }
+                            }))
+                            .font(.title)
+                            .padding()
+                            Spacer()
+                        }
 
-                                TextField("Name", text: Binding(get: { externalName.name }, set: { update(externalName: $0) }))
+                        HStack {
+                            Menu {
+                                Button(action: setExternalNameAsName, label: { Text("Name") })
+                                Button(action: setExternalNameAsProduct, label: { Text("product") })
+                            } label: {
+                                Text(externalNameMenuTitle)
+                            }
+                            .frame(width: 100)
+
+                            TextField("Name", text: Binding(get: { externalName.name }, set: { update(externalName: $0) }))
+                                .font(.largeTitle)
+                                .padding()
+                            if case .product(_, let package) = externalName {
+                                TextField("Package", text: Binding(get: { package }, set: { update(externalProduct: $0) }))
                                     .font(.largeTitle)
                                     .padding()
-                                if case .product(_, let package) = externalName {
-                                    TextField("Package", text: Binding(get: { package }, set: { update(externalProduct: $0) }))
-                                        .font(.largeTitle)
-                                        .padding()
-                                }
-                            }
-                            
-                            Button(action: { store.addRemoteDependencyToSelectedComponent(dependency: RemoteDependency(url: externalURL,
-                                                                                                                       name: externalName,
-                                                                                                                       value: externalDescription)) }) {
-                                Text("Add Remote Dependency")
                             }
                         }
+
+                        Button(action: { store.addRemoteDependencyToSelectedComponent(dependency: RemoteDependency(url: externalURL,
+                                                                                                                   name: externalName,
+                                                                                                                   value: externalDescription)) }) {
+                            Text("Add Remote Dependency")
+                        }
                     }
-                    .padding()
                 }
-                Button(action: { showingPopup = false }, label: { Text("Cancel") })
+                .padding()
             }
+            Button(action: { showingPopup = false },
+                   label: { Text("Cancel").font(.largeTitle).padding() })
             .padding()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
