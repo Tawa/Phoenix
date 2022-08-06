5632bbf9a4b5d74686887cb1ebc9f3056bd4de28
Tawa <tawanicolas@gmail.com>
2022-08-06 15:05:59 +0200

    Migrate from Resolver to Factory

--- Phoenix/CompositionRoot/CompositionRoot.swift
+++ Phoenix/CompositionRoot/CompositionRoot.swift
@@ -3,39 +3,33 @@ import AppVersionProviderContract
 import AppVersionProvider
 import DocumentCoderContract
 import DocumentCoder
-import Resolver
+import Factory
+
+extension Container {
+    static let currentAppVersionStringProvider = Factory { Bundle.main as CurrentAppVersionStringProviderProtocol }
+    static let appVersionStringParser = Factory { AppVersionStringParser() as AppVersionStringParserProtocol }
+    static let currentAppVersionProvider = Factory {
+        CurrentAppVersionProvider(
+            appVersionStringProvider: Container.currentAppVersionStringProvider(),
+            appVersionStringParser: Container.appVersionStringParser()
+        ) as CurrentAppVersionProviderProtocol
+    }
+
+    static let phoenixDocumentFileWrappersDecoder = Factory {
+        PhoenixDocumentFileWrappersDecoder(
+            appVersionStringParser: Container.appVersionStringParser()
+        ) as PhoenixDocumentFileWrappersDecoderProtocol
+    }
+
+    static let phoenixDocumentFileWrapperEncoder = Factory {
+        PhoenixDocumentFileWrapperEncoder(
+            currentAppVersionStringProvider: Container.currentAppVersionStringProvider()
+        ) as PhoenixDocumentFileWrapperEncoderProtocol
+    }
+}
 
 extension Bundle: CurrentAppVersionStringProviderProtocol {
     public func currentAppVersionString() -> String? {
         infoDictionary?["CFBundleShortVersionString"] as? String
-    }
-}
-
-extension Resolver: ResolverRegistering {
-    public static func registerAllServices() {
-        register {
-            Bundle.main
-        }.implements(CurrentAppVersionStringProviderProtocol.self)
-
-        register {
-            AppVersionStringParser()
-        }.implements(AppVersionStringParserProtocol.self)
-
-        register {
-            CurrentAppVersionProvider(appVersionStringProvider: resolve(),
-                                      appVersionStringParser: resolve())
-        }.implements(CurrentAppVersionProviderProtocol.self)
-
-        register { resolver in
-            PhoenixDocumentFileWrappersDecoder(
-                appVersionStringParser: resolver.resolve()
-            )
-        }.implements(PhoenixDocumentFileWrappersDecoderProtocol.self)
-
-        register { resolver in
-            PhoenixDocumentFileWrapperEncoder(
-                currentApp: resolver.resolve()
-            )
-        }.implements(PhoenixDocumentFileWrapperEncoderProtocol.self)
     }
 }
