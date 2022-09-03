92f7627a79f5f20ae1fc2e9d2cb5015453f4d8e0
Tawa <tawanicolas@gmail.com>
2022-09-03 15:23:08 +0200

    Add Older versions of macOS

--- Modules/Entities/Package/Sources/Package/Domain/Package/MacOSVersion.swift
+++ Modules/Entities/Package/Sources/Package/Domain/Package/MacOSVersion.swift
@@ -1,5 +1,12 @@
 public enum MacOSVersion: String, Codable, Hashable, CaseIterable, Identifiable {
     public var id: Int { hashValue }
 
+    case v10_10
+    case v10_11
+    case v10_12
+    case v10_13
+    case v10_14
+    case v10_15
+    case v11
     case v12
 }
