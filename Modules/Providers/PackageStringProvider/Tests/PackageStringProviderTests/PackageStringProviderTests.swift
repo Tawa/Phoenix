2149d5ece053f626b16aa794583b69bef4a97d79
Tawa <tawanicolas@gmail.com>
2022-11-17 20:10:57 +0100

    Update Unit Tests

--- Modules/Providers/PackageStringProvider/Tests/PackageStringProviderTests/PackageStringProviderTests.swift
+++ Modules/Providers/PackageStringProvider/Tests/PackageStringProviderTests/PackageStringProviderTests.swift
@@ -7,37 +7,38 @@ final class PackageStringProviderTests: XCTestCase {
     func testPackage() {
         // Given
         let package = SwiftPackage(name: "HomeService",
-                              iOSVersion: nil,
-                              macOSVersion: nil,
-                              products: [
-                                .library(Library(name: "HomeService", type: .undefined, targets: ["HomeService"]))
-                              ],
-                              dependencies: [
-                                .module(path: "../../Contracts/Services/HomeServiceContract", name: "HomeServiceContract"),
-                                .module(path: "../../Contracts/Repositories/HomeRepositoryContract", name: "HomeRepositoryContract"),
-                                .module(path: "../../Support/DI", name: "DI")
-                              ],
-                              targets: [
-                                Target(name: "HomeService",
-                                       dependencies: [
-                                        .module(path: "../../Contracts/Services/HomeServiceContract", name: "HomeServiceContract"),
-                                        .module(path: "../../Contracts/Repositories/HomeRepositoryContract", name: "HomeRepositoryContract"),
-                                        .module(path: "../../Support/DI", name: "DI")
-                                       ],
-                                       isTest: false,
-                                      resources: []),
-                                Target(name: "HomeServiceTests",
-                                       dependencies: [ .module(path: "", name: "HomeService") ],
-                                       isTest: true,
-                                      resources: [])
-                              ],
-                              swiftVersion: "5.7")
-
+                                   defaultLocalization: nil,
+                                   iOSVersion: nil,
+                                   macOSVersion: nil,
+                                   products: [
+                                    .library(Library(name: "HomeService", type: .undefined, targets: ["HomeService"]))
+                                   ],
+                                   dependencies: [
+                                    .module(path: "../../Contracts/Services/HomeServiceContract", name: "HomeServiceContract"),
+                                    .module(path: "../../Contracts/Repositories/HomeRepositoryContract", name: "HomeRepositoryContract"),
+                                    .module(path: "../../Support/DI", name: "DI")
+                                   ],
+                                   targets: [
+                                    Target(name: "HomeService",
+                                           dependencies: [
+                                            .module(path: "../../Contracts/Services/HomeServiceContract", name: "HomeServiceContract"),
+                                            .module(path: "../../Contracts/Repositories/HomeRepositoryContract", name: "HomeRepositoryContract"),
+                                            .module(path: "../../Support/DI", name: "DI")
+                                           ],
+                                           isTest: false,
+                                           resources: []),
+                                    Target(name: "HomeServiceTests",
+                                           dependencies: [ .module(path: "", name: "HomeService") ],
+                                           isTest: true,
+                                           resources: [])
+                                   ],
+                                   swiftVersion: "5.7")
+        
         let sut = PackageStringProvider()
-
+        
         // When
         let packageString = sut.string(for: package)
-
+        
         // Then
         XCTAssertEqual(packageString, """
 // swift-tools-version: 5.7
