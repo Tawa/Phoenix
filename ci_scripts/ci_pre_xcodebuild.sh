c2343856941e69370a9e975e4c2aaf2b3c2a5850
Tawa <tawanicolas@gmail.com>
2023-03-05 11:08:30 +0100

    Enable script only for Unit Tests

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -6,14 +6,14 @@
 #  Created by Tawa Nicolas on 05.03.23.
 #  
 
-#if [[ $CI_WORKFLOW == "PR Unit Tests" ]];
-#then
+if [[ $CI_WORKFLOW == "PR Unit Tests" ]];
+then
     echo "Validating \"Package.swift\" files"
     cmd="./ProjectValidatorCommand ../Modules/Modules.ash ../Modules"
     $cmd
     status=$?
     [ $status -eq 0 ] && echo "$cmd command was successful" || echo "$cmd failed"
     exit $status
-#else
-#    echo "ci_pre_xcodebuild No Actions"
-#fi
+else
+    echo "ci_pre_xcodebuild No Actions"
+fi
