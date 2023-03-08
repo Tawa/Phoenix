88e944f008c83b6da87a9ac9a49f7a815a4cf9bd
Tawa <tawanicolas@gmail.com>
2023-03-08 12:52:06 +0100

    Update ci_scripts [ci skip]

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -5,5 +5,5 @@
 
 if [[ $CI_WORKFLOW == "GitHub Release" ]];
 then
-    "./CreateGitHubRelease.swift" "$CI_TAG" "$CI_ARCHIVE_PATH" "$GITHUB_TOKEN"
+    "$CI_WORKSPACE/ci_scripts/CreateGitHubRelease.swift" "$CI_TAG" "$CI_ARCHIVE_PATH" "$GITHUB_TOKEN"
 fi
--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -6,6 +6,6 @@
 if [[ $CI_WORKFLOW == "GitHub Release" ]];
 then
     # Replace entitlements to disable Sandbox for the GitHub Release
-    cp -fr "../Phoenix/Phoenix.entitlements" "../Phoenix/PhoenixRelease.entitlements"
-    "./EnableGitHubReleaseTarget.swift" "../Phoenix.xcodeproj/xcshareddata/xcschemes/Phoenix.xcscheme"
+    cp -fr "$CI_WORKSPACE/Phoenix/Phoenix.entitlements" "$CI_WORKSPACE/Phoenix/PhoenixRelease.entitlements"
+    "$CI_WORKSPACE/ci_scripts/EnableGitHubReleaseTarget.swift" "$CI_WORKSPACE/Phoenix.xcodeproj/xcshareddata/xcschemes/Phoenix.xcscheme"
 fi
