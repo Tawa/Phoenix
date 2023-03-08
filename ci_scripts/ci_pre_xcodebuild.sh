ab09688bc538eaef81b4b9eee7a07240391ad0e1
Tawa <tawanicolas@gmail.com>
2023-03-08 12:15:39 +0100

    Update ci_pre_xcodebuild.sh [ci skip]

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -6,6 +6,6 @@
 if [[ $CI_WORKFLOW == "GitHub Release" ]];
 then
     # Replace entitlements to disable Sandbox for the GitHub Release
-    cp -fr Phoenix/Phoenix.entitlements Phoenix/PhoenixRelease.entitlements
+    cp -fr "../Phoenix/Phoenix.entitlements" "../Phoenix/PhoenixRelease.entitlements"
     "./EnableGitHubReleaseTarget.swift" "../Phoenix.xcodeproj/xcshareddata/xcschemes/Phoenix.xcscheme"
 fi
