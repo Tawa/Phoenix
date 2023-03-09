c2a9e7e3580a22d9ed8c643a421127d62be0c969
Tawa <tawanicolas@gmail.com>
2023-03-09 22:51:25 +0100

    Fix zipping paths [ci skip]

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -7,8 +7,8 @@ if [[ $CI_WORKFLOW == "GitHub Release" ]];
 then
     ZIP_PATH="$CI_WORKSPACE/ci_scripts/Phoenix.app.zip"
 
-    cd "$ARCHIVE_PATH/Products/Applications/"
-    zip -r $ARCHIVE_PATH Phoenix.app
+    cd "$CI_ARCHIVE_PATH/Products/Applications/"
+    zip -r $ZIP_PATH Phoenix.app
     
     "$CI_WORKSPACE/ci_scripts/CreateGitHubRelease.swift" $CI_TAG $ZIP_PATH $GITHUB_TOKEN
 fi
